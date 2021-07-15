aa22c4d3dc4b825edbbb7b6c47b362a4ba868bd6
Yakihavi <84100345+Yakihavi@users.noreply.github.com>
2021-07-15 19:43:51 +0300

    Add files via upload

---
+++ dz_1_3.py
@@ -0,0 +1,7 @@
+for numb in range(1, 21):
+    if numb == 1:
+        print(str(numb) + " процент")
+    elif numb < 5:
+        print(str(numb) + " процента")
+    else:
+        print(str(numb) + " процентов")
